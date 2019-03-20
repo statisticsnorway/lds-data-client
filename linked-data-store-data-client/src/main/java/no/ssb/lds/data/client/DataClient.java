efec5918315a7a07b3b3a8073d6bf54d6e7202eb
Hadrien Kohl <hadrien.kohl@gmail.com>
2019-03-20 09:14:00 +0100

    Improve DataClient

--- linked-data-store-data-client/src/main/java/no/ssb/lds/data/client/DataClient.java
+++ linked-data-store-data-client/src/main/java/no/ssb/lds/data/client/DataClient.java
@@ -3,12 +3,15 @@ package no.ssb.lds.data.client;
 import io.reactivex.Completable;
 import io.reactivex.Flowable;
 import no.ssb.lds.data.common.BinaryBackend;
-import no.ssb.lds.data.common.converter.FormatConverter;
+import no.ssb.lds.data.common.parquet.ParquetProvider;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericRecord;
+import org.apache.parquet.hadoop.ParquetReader;
+import org.apache.parquet.hadoop.ParquetWriter;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.channels.SeekableByteChannel;
 import java.util.List;
 import java.util.Objects;
 
@@ -21,7 +24,7 @@ import java.util.Objects;
  * <p>
  * One can also read and write {@link GenericRecord}s.
  */
-public abstract class DataClient {
+public class DataClient {
 
     protected final Configuration configuration;
 
@@ -39,19 +42,37 @@ public abstract class DataClient {
      * @param token     an authentication token.
      * @return a completable that completes once the data is saved.
      */
-    public abstract Completable convertAndWrite(String dataId, Schema schema, InputStream input, String mediaType,
-                                                String token);
+    public Completable convertAndWrite(String dataId, Schema schema, InputStream input, String mediaType,
+                                       String token) {
+        for (FormatConverter converter : configuration.getConverters()) {
+            if (converter.doesSupport(mediaType)) {
+                Flowable<GenericRecord> records = converter.read(input, mediaType, schema);
+                return writeData(dataId, schema, records, token);
+            }
+        }
+        return Completable.error(new IllegalArgumentException("unsupported type " + mediaType));
+    }
 
     /**
      * Read data and convert to binary data.
      *
-     * @param dataId    an opaque identifier for the data.
-     * @param schema    the schema used to parse the data.
-     * @param mediaType the media type of the binary data.
-     * @param token     an authentication token.
-     * @return a completable that completes once the data is saved.
+     * @param dataId       an opaque identifier for the data.
+     * @param schema       the schema used to parse the data.
+     * @param outputStream the output stream to read the data into.
+     * @param mediaType    the media type of the binary data.
+     * @param token        an authentication token.
+     * @return a completable that completes once the data is read.
      */
-    public abstract OutputStream readAndConvert(String dataId, Schema schema, String mediaType, String token);
+    public Completable readAndConvert(String dataId, Schema schema, OutputStream outputStream,
+                                      String mediaType, String token) {
+        for (FormatConverter converter : configuration.getConverters()) {
+            if (converter.doesSupport(mediaType)) {
+                Flowable<GenericRecord> records = readData(dataId, schema, token);
+                return converter.write(records, outputStream, mediaType, schema);
+            }
+        }
+        throw new IllegalArgumentException("unsupported type " + mediaType);
+    }
 
     /**
      * Write a sequence of {@link GenericRecord}s to the bucket storage.
@@ -62,7 +83,13 @@ public abstract class DataClient {
      * @param token   an authentication token.
      * @return a completable that completes once the data is saved.
      */
-    public abstract Completable writeData(String dataId, Schema schema, Flowable<GenericRecord> records, String token);
+    public Completable writeData(String dataId, Schema schema, Flowable<GenericRecord> records, String token) {
+        BinaryBackend backend = configuration.getBackend();
+        ParquetProvider provider = configuration.getParquetProvider();
+        SeekableByteChannel writableChannel = backend.write(dataId);
+        ParquetWriter<GenericRecord> parquetWriter = provider.getWriter(writableChannel, schema);
+        return records.doOnNext(record -> parquetWriter.write(record)).ignoreElements();
+    }
 
     /**
      * Read a sequence of {@link GenericRecord}s from the bucket storage.
@@ -72,15 +99,37 @@ public abstract class DataClient {
      * @param token  an authentication token.
      * @return a {@link Flowable} of records.
      */
-    public abstract Flowable<GenericRecord> readData(String dataId, Schema schema, String token);
+    public Flowable<GenericRecord> readData(String dataId, Schema schema, String token) {
+        BinaryBackend backend = configuration.getBackend();
+        ParquetProvider provider = configuration.getParquetProvider();
+        SeekableByteChannel readableChannel = backend.read(dataId);
+        ParquetReader<GenericRecord> parquetReader = provider.getReader(readableChannel, schema);
+        return Flowable.generate(emitter -> {
+            GenericRecord read = parquetReader.read();
+            if (read == null) {
+                emitter.onComplete();
+            } else {
+                emitter.onNext(read);
+            }
+        });
+    }
 
     public static class Configuration {
 
         private final BinaryBackend backend;
         private List<FormatConverter> converters;
+        private ParquetProvider parquetProvider;
 
         public Configuration(BinaryBackend backend) {
             this.backend = backend;
+        }
+
+        public ParquetProvider getParquetProvider() {
+            return parquetProvider;
+        }
+
+        public void setParquetProvider(ParquetProvider parquetProvider) {
+            this.parquetProvider = parquetProvider;
         }
 
         public BinaryBackend getBackend() {
---
+++ linked-data-store-data-client/src/main/java/no/ssb/lds/data/client/FormatConverter.java
@@ -0,0 +1,29 @@
+package no.ssb.lds.data.client;
+
+import io.reactivex.Completable;
+import io.reactivex.Flowable;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericRecord;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public interface FormatConverter {
+
+    /**
+     * Returns true if this converter can handle the media type.
+     */
+    boolean doesSupport(String mediaType);
+
+    String getMediaType();
+
+    /**
+     * Convert the input stream to {@link GenericRecord}s.
+     */
+    Flowable<GenericRecord> read(InputStream input, String mimeType, Schema schema);
+
+    /**
+     * Convert the {@link Flowable<GenericRecord>} to the output stream.
+     */
+    Completable write(Flowable<GenericRecord> records, OutputStream output, String mimeType, Schema schema);
+}
